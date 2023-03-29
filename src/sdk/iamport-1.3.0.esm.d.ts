a2a47d9fd0c48600dda0d7fc6be8e12ca7ccd086
deedee <mymusese@gmail.com>
2023-03-30 02:40:02 +0900

    feat: CheckoutService 리팩토링 및 페이팔 작업으로 인해 breaking changes가 발생해 벤더링 하고 있던 1.3.0 / 2.0.0 코드 수정

--- src/sdk/iamport-1.3.0.esm.d.ts
+++ src/sdk/iamport-1.3.0.esm.d.ts
@@ -2,5 +2,7 @@ const IMP: any;
 export const slots: {
   CORE_SERVER: string;
   CHECKOUT_SERVER: string;
+  PG_DRIVERS_SERVE_URL: string;
+  SDK_VERSION: string;
 };
 export default IMP;
--- src/sdk/iamport-2.0.0.esm.d.ts
+++ src/sdk/iamport-2.0.0.esm.d.ts
@@ -2,5 +2,7 @@ const PortOne: any;
 export const slots: {
   CORE_SERVER: string;
   CHECKOUT_SERVER: string;
+  PG_DRIVERS_SERVE_URL: string;
+  SDK_VERSION: string;
 };
 export default PortOne;
