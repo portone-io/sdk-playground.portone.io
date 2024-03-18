223dfe7a730592da13c75efbd310a13609fff81f
XiNiHa <me@xiniha.dev>
2024-03-18 18:47:55 +0900

    파일 타입정의 추가

---
+++ src/files.d.ts
@@ -0,0 +1,22 @@
+declare module "*/trial.yaml" {
+  type TrialYaml = {
+    label: string;
+    icon: string;
+    [
+      _:
+        | "v1-cert"
+        | "v1-pay"
+        | "v1-load-ui"
+        | "v2-cert"
+        | "v2-pay"
+        | "v2-load-ui"
+    ]: {
+      account: Record<string, unknown>;
+      field: Record<string, unknown>;
+      case: Record<string, Record<string, unknown>>;
+    };
+  }[];
+
+  const value: TrialYaml;
+  export default value;
+}
