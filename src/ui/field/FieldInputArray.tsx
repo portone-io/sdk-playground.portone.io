c231b497e6800e7adb6094f594e671d20160c84f
CirnoV (Sickle) <cirnov59@gmail.com>
2024-07-18 17:23:07 +0900

    lint 수정

--- src/ui/field/FieldInputArray.tsx
+++ src/ui/field/FieldInputArray.tsx
@@ -8,39 +8,40 @@ import {
 import { fieldInputComponents } from "./FieldControl";
 import type { FieldInputProps } from "./input";
 
-const FieldInputArray: React.FC<FieldInputProps<ArrayInput, ArrayFieldSignal>> =
-	({ fieldInput, fieldSignal }) => {
-		const fieldSignals = fieldSignal.valueSignal.value;
-		const FieldInput = fieldInputComponents[fieldInput.inputItem.type];
-		return (
-			<div className="flex flex-col items-start gap-2">
-				{fieldSignals.map((itemSignal: FieldSignal, i: number) => (
-					<div key={fieldSignal.getKey(i)} className="flex items-start gap-2">
-						<FieldInput
-							fieldInput={fieldInput.inputItem}
-							fieldSignal={itemSignal}
-						/>
-						<button
-							type="button"
-							onClick={() => {
-								fieldSignal.remove(i);
-								fieldSignal.enabledSignal.value = true;
-							}}
-						>
-							➖
-						</button>
-					</div>
-				))}
-				<button
-					type="button"
-					onClick={() => {
-						fieldSignal.append();
-						fieldSignal.enabledSignal.value = true;
-					}}
-				>
-					➕
-				</button>
-			</div>
-		);
-	};
+const FieldInputArray: React.FC<
+	FieldInputProps<ArrayInput, ArrayFieldSignal>
+> = ({ fieldInput, fieldSignal }) => {
+	const fieldSignals = fieldSignal.valueSignal.value;
+	const FieldInput = fieldInputComponents[fieldInput.inputItem.type];
+	return (
+		<div className="flex flex-col items-start gap-2">
+			{fieldSignals.map((itemSignal: FieldSignal, i: number) => (
+				<div key={fieldSignal.getKey(i)} className="flex items-start gap-2">
+					<FieldInput
+						fieldInput={fieldInput.inputItem}
+						fieldSignal={itemSignal}
+					/>
+					<button
+						type="button"
+						onClick={() => {
+							fieldSignal.remove(i);
+							fieldSignal.enabledSignal.value = true;
+						}}
+					>
+						➖
+					</button>
+				</div>
+			))}
+			<button
+				type="button"
+				onClick={() => {
+					fieldSignal.append();
+					fieldSignal.enabledSignal.value = true;
+				}}
+			>
+				➕
+			</button>
+		</div>
+	);
+};
 export default FieldInputArray;
--- src/ui/field/FieldInputUnion.tsx
+++ src/ui/field/FieldInputUnion.tsx
@@ -5,52 +5,51 @@ import Control from "../Control";
 import FieldControl, { fieldInputComponents } from "./FieldControl";
 import type { FieldInputProps } from "./input";
 
-const FieldInputUnion: React.FC<FieldInputProps<UnionInput, UnionFieldSignal>> =
-	({ fieldInput, fieldSignal }) => {
-		const fieldSignals = fieldSignal.valueSignal.value;
-		const radioGroup = `field-input-union-${Math.random()
-			.toString(36)
-			.slice(2)}`;
-		fieldSignal.activeKeySignal.subscribe((activeKey) => {
-			batch(() => {
-				for (const [key, fieldSignal] of Object.entries(fieldSignals)) {
-					if (key === activeKey) {
-						fieldSignal.enabledSignal.value = true;
-					} else {
-						fieldSignal.enabledSignal.value = false;
-					}
+const FieldInputUnion: React.FC<
+	FieldInputProps<UnionInput, UnionFieldSignal>
+> = ({ fieldInput, fieldSignal }) => {
+	const fieldSignals = fieldSignal.valueSignal.value;
+	const radioGroup = `field-input-union-${Math.random().toString(36).slice(2)}`;
+	fieldSignal.activeKeySignal.subscribe((activeKey) => {
+		batch(() => {
+			for (const [key, fieldSignal] of Object.entries(fieldSignals)) {
+				if (key === activeKey) {
+					fieldSignal.enabledSignal.value = true;
+				} else {
+					fieldSignal.enabledSignal.value = false;
 				}
-			});
+			}
 		});
-		return (
-			<div className="flex flex-col gap-2">
-				{Object.entries(fieldInput.fields).map(([key, field]) => {
-					const FieldInput = fieldInputComponents[field.input.type];
-					const required = import.meta.env.VITE_SDK_PREVIEW
-						? false
-						: field.required;
-					return (
-						<Control
-							key={key}
-							code={key}
-							label={field.label}
-							required={required}
-							enabled={fieldSignals[key].enabledSignal.value}
-							onToggle={(value) => {
-								if (value === true) {
-									fieldSignal.activeKeySignal.value = key;
-								}
-							}}
-							radioGroup={radioGroup}
-						>
-							<FieldInput
-								fieldInput={field.input}
-								fieldSignal={fieldSignals[key]}
-							/>
-						</Control>
-					);
-				})}
-			</div>
-		);
-	};
+	});
+	return (
+		<div className="flex flex-col gap-2">
+			{Object.entries(fieldInput.fields).map(([key, field]) => {
+				const FieldInput = fieldInputComponents[field.input.type];
+				const required = import.meta.env.VITE_SDK_PREVIEW
+					? false
+					: field.required;
+				return (
+					<Control
+						key={key}
+						code={key}
+						label={field.label}
+						required={required}
+						enabled={fieldSignals[key].enabledSignal.value}
+						onToggle={(value) => {
+							if (value === true) {
+								fieldSignal.activeKeySignal.value = key;
+							}
+						}}
+						radioGroup={radioGroup}
+					>
+						<FieldInput
+							fieldInput={field.input}
+							fieldSignal={fieldSignals[key]}
+						/>
+					</Control>
+				);
+			})}
+		</div>
+	);
+};
 export default FieldInputUnion;
