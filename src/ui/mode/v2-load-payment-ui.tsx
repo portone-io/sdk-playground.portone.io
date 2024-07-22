88018341bf52e642c83c656c3c9c5de879fb2ff1
CirnoV (Sickle) <cirnov59@gmail.com>
2024-07-22 17:37:01 +0900

    CodeMirror에서 readOnly 대신 editable을 사용하도록 수정

--- src/ui/mode/v2-load-payment-ui.tsx
+++ src/ui/mode/v2-load-payment-ui.tsx
@@ -84,7 +84,7 @@ const View: React.FC = () => {
 						<div className="flex-1">
 							<HtmlEditor
 								className="h-full"
-								readOnly
+								editable={false}
 								value={codePreviewSignal.value}
 							/>
 						</div>
